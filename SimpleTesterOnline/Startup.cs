ecb029d68d0fc8c15415be4adb484b63418c9e7f
Krzysztof Dubanowicz <thekrzyko@gmail.com>
2019-12-03 20:10:06 +0100

    Add project files.

---
+++ SimpleTesterOnline/Startup.cs
@@ -0,0 +1,20 @@
+using Blazor.FileReader;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace SimpleTesterOnline
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
